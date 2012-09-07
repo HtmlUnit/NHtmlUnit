89ded7692d9dd8104af228606d312f125605bcf1
asbjornu <asbjorn@ulsberg.no>
2012-09-07 09:01:08 +0200

    Updated Make.cmd to accommodate the path changes in the solution

--- lib/HtmlUnit/Make.cmd
+++ lib/HtmlUnit/Make.cmd
@@ -1,14 +1,14 @@
-rem @echo off
+@echo off
 : The version should be set to the same as that of the Java version of HtmlUnit
-set VERSION=2.9.0.0
+set VERSION=2.10.0.0
 
 echo Please be patient while compiling HtmlUnit JAR files into HtmlUnit.dll.
 echo There will be a lot of warnings; these can be safely ignored.
 echo.
 echo.
 
-set JAVAC="C:\Program Files\Java\jdk1.7.0\bin\javac.exe"
-set IKVM=..\IKVM\_bin
+set JAVAC="C:\Program Files\Java\jdk1.7.0_03\bin\javac.exe"
+set IKVM=..\..\packages\IKVM.NET.7.0.4335.1\lib
 set IKVMC="%IKVM%\ikvmc.exe"
 set IKVMSTUB="%IKVM%\ikvmstub.exe"
 
@@ -22,19 +22,19 @@ if exist %JAVAC% (
         echo.
         %JAVAC% -cp mscorlib.jar AssemblyInfo.java
 
-        REM echo Compiling HtmlUnit.jar to HtmlUnit.dll (x64)
-        REM echo.
-        REM %IKVMC% -out:.\x64\HtmlUnit.dll -assembly:HtmlUnit -platform:x64 -version:%VERSION% -reference:mscorlib.dll assembly.class "..\..\tools\HtmlUnit\lib\*.jar"
+        rem echo Compiling HtmlUnit.jar to HtmlUnit.dll [x64]
+        rem echo.
+        rem %IKVMC% -out:.\x64\HtmlUnit.dll -assembly:HtmlUnit -platform:x64 -version:%VERSION% -reference:mscorlib.dll assembly.class "lib\*.jar"
 
-        REM echo Compiling HtmlUnit.jar to HtmlUnit.dll (x86)
-        REM echo.
-        REM %IKVMC% -out:.\x86\HtmlUnit.dll -assembly:HtmlUnit -platform:x86 -version:%VERSION% -reference:mscorlib.dll assembly.class "..\..\tools\HtmlUnit\lib\*.jar"
+        rem echo Compiling HtmlUnit.jar to HtmlUnit.dll [x86]
+        rem echo.
+        rem %IKVMC% -out:.\x86\HtmlUnit.dll -assembly:HtmlUnit -platform:x86 -version:%VERSION% -reference:mscorlib.dll assembly.class "lib\*.jar"
 
-        echo Compiling HtmlUnit.jar to HtmlUnit.dll (any)
+        echo Compiling HtmlUnit.jar to HtmlUnit.dll [AnyCpu]
         echo.
-        %IKVMC% -out:.\anycpu\HtmlUnit.dll -assembly:HtmlUnit -platform:anycpu -version:%VERSION% -reference:mscorlib.dll assembly.class "..\..\tools\HtmlUnit\lib\*.jar"
+        %IKVMC% -out:.\anycpu\HtmlUnit.dll -assembly:HtmlUnit -platform:anycpu -version:%VERSION% -reference:mscorlib.dll assembly.class "lib\*.jar"
     ) else (
-        echo %IKVM% not found.
+        echo %IKVMC% not found.
     )
 ) else (
     echo %JAVAC% not found.
